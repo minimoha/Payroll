b3e557b1d39c4a6f456eea521ca05a9ab1307b72
Don Clovis <cloviso@novohealthafrica.org>
2020-02-23 18:48:29 +0100

    Pagination

--- Paycompute/Controllers/EmployeeController.cs
+++ Paycompute/Controllers/EmployeeController.cs
@@ -21,7 +21,7 @@ namespace Paycompute.Controllers
             _hostingEnvironment = hostingEnvironment;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int? pageNumber)
         {
             var employees = _employeeService.GetAll().Select(employee => new EmployeeIndexViewModel
             {
@@ -34,8 +34,8 @@ namespace Paycompute.Controllers
                 City = employee.City,
                 DateJoined = employee.DateJoined
             }).ToList();
-          
-            return View(employees);
+            int pageSize = 4;
+            return View(EmployeeListPagination<EmployeeIndexViewModel>.Create(employees, pageNumber ?? 1, pageSize));
         }
 
         [HttpGet]
